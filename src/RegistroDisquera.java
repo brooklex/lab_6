a584aa3a4f3fccc5045fcf1f7d509a16c7475433
brooklex <dayane.sepulveda2401@alumnos.ubiobio.cl>
2026-05-05 16:29:28 -0400

    lab 6

--- src/RegistroDisquera.java
+++ src/RegistroDisquera.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegistroDisquera {
+    private static RegistroDisquera instancia;
+
+    private List<String>artistas;
+
+    private RegistroDisquera(){
+        artistas=new ArrayList<>();
+    }
+    public static RegistroDisquera getInstance(){
+        if (instancia==null){
+            instancia=new RegistroDisquera();
+        }
+        return instancia;
+    }
+    public void registrarArtista(String nombre){
+        artistas.add(nombre);
+    }
+
+    public void mostrarArtistas(){
+        System.out.println("Artistas registrados");
+        for (String artista:artistas){
+            System.out.println("-"+artista);
+        }
+    }
 }
+
